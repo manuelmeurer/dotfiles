50c24cc8ad841444c9dd82004fb0ef4f573a1be3
Manuel Meurer <manuel.meurer@gmail.com>
2012-08-04 13:22:35 +0200

    Improve link.rb

--- link.rb
+++ link.rb
@@ -1,12 +1,18 @@
+#!/usr/bin/env ruby
+
+ignore = [
+  File.basename(__FILE__)
+]
+
 Dir['*'].each do |f|
-  next if f =~ /\.tmproj$/
+  next if ignore.any? { |i| i === f }
 
   file    = File.expand_path("./#{f}")
   dotfile = File.expand_path("~/.#{f}")
 
   if File.exists?(dotfile) || File.symlink?(dotfile)
     print "File #{dotfile} already exists. Overwrite? (yN) "
-    next unless gets == "y\n"
+    next unless gets.chomp == "y"
     File.delete(dotfile)
   end
 
