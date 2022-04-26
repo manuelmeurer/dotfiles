18d274a4ffea491683e74f56e8b915e2531d0459
Manuel Meurer <manuel@meurer.io>
2022-04-26 22:03:40 +0200

    no special handling for sublime files

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for file in *; do
-  if [[ $file =~ .+\.sublime.+ ]] || [[ $file = $(basename -- $0) ]]; then
+  if [[ $file = $(basename -- $0) ]]; then
     continue
   fi
   ln -sf "$PWD/$file" "$HOME/.$file"
